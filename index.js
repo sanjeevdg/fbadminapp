ae44e57f1528ccc6fc6ad64a5dc44e27af2c0403
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2024-12-12 09:40:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,19 @@ serviceAccount.private_key = serviceAccount.private_key.split(String.raw`\n`).jo
 
 // Intialize the firebase-admin project/account
 admin.initializeApp({
-  credential: admin.credential.cert({...serviceAccount,serviceAccount.private_key})
+  credential: admin.credential.cert({
+  "type": serviceAccount.type,
+  "project_id": serviceAccount.project_id,
+  "private_key_id": serviceAccount.private_key_id,
+  "private_key": serviceAccount.private_key,
+  "client_email": serviceAccount.client_email,
+  "client_id": serviceAccount.client_id,
+  "auth_uri":serviceAccount.auth_uri,
+  "token_uri": serviceAccount.token_uri,
+  "auth_provider_x509_cert_url": serviceAccount.auth_provider_x509_cert_url,
+  "client_x509_cert_url":serviceAccount.client_x509_cert_url,
+  "universe_domain": serviceAccount.universe_domain
+})
 });
 
 app.use(cors());
